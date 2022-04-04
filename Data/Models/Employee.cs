3e48e52e07f55f6050cb1818583389be17651aa8
marcus P <mejl2marcuspahlsson@hotmail.com>
2022-04-04 17:04:33 +0200

    Wip

---
+++ Data/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    internal class Employee
+    {
+        [Key] public string EmployeeId { get; set; }
+
+        public string Email { get; set; }
+        [RegularExpression(@"^[A-Öa-ö ]+$", ErrorMessage = "Names can only have letters and space")]
+        public string Fullname { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "Adress can only have letters, numbers and space")]
+        public ICollection<EmployeeInOrder> ActiveInOrders { get; set; }
+    }
+}
---
+++ Data/Models/EmployeeInOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    internal class EmployeeInOrder
+    {
+        [Key, ForeignKey("Profile"), Column(Order = 0)]
+        public string ProfileId { get; set; }
+
+        [Key, ForeignKey("Project"), Column(Order = 1)]
+        public int ProjectID { get; set; }
+
+
+        public virtual Profile Profile { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
