e69d4fc23186aa59d5f29aa24dbde4998265b7f7
marcus P <mejl2marcuspahlsson@hotmail.com>
2022-04-05 11:01:51 +0200

    Lägger till så man kan skapa customer och fixar typo

---
+++ HatHut22/Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using CVSITE21.Data;
+using Data.Migrations;
+using Data.Models;
+using Microsoft.AspNet.Identity;
+
+namespace HatHut22.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult CreateCustomers()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+        
+                using (var context = new ApplicationDbContext())
+                {
+                    if (ModelState.IsValid)
+                    {
+                        context.Customers.Add(customer);
+                        context.SaveChanges();
+                        return RedirectToAction("List");
+                    }
+
+                    return RedirectToAction("List");
+                }
+
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
