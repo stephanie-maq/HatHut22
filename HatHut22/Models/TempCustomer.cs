1f344b2e96743630818deed95ae8ec13ffc3ab49
marcus P <mejl2marcuspahlsson@hotmail.com>
2022-04-06 23:48:32 +0200

    wip

---
+++ HatHut22/Models/TempCustomer.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatHut22.Models
+{
+    public class TempCustomer
+    {
+        public int CostumerId { get; set; }
+        public string Email { get; set; }
+        public string Fullname { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+        public int phoneNumber { get; set; }
+        public List<string> OwnerOfOrders { get; set; }
+    }
+}
