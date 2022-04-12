61fd89ca40fd07ca8105d518cf4cc9ca83737c4a
eddero <edvin97johansson@hotmail.com>
2022-04-12 13:03:29 +0200

    order metho

--- HatHut22/Controllers/OrdersController.cs
+++ HatHut22/Controllers/OrdersController.cs
@@ -179,6 +179,34 @@ namespace HatHut22.Controllers
             }            
         }
 
+        public ActionResult PrintOrder(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var context = new ApplicationDbContext())
+            {
+                var order = db.Orders.Find(id);
+                var productID = order.OrderProductId;
+                var customerId = order.OrderCustomerId;
+                var product = context.Products.FirstOrDefault(x => x.productId == productID);
+                var customer = context.Customers.FirstOrDefault(x => x.CostumerId == customerId);
+                ViewBag.ProductPrice = product.Price;
+                ViewBag.ProductTitle = product.Title;
+                ViewBag.ProductDescription = product.Description;
+                ViewBag.CustomerAddress = customer.Address;
+                ViewBag.CustomerEmail = customer.Email;
+                ViewBag.CustomerName = customer.Fullname;
+                ViewBag.CustomerPhone = customer.phoneNumber;
+
+                //ViewBag.OrderEmployeeId = new SelectList(db.Employees, "EmployeeId", "Email");
+                //ViewBag.OrderCustomerId = new SelectList(db.Customers, "CostumerId", "Email");
+                //ViewBag.OrderProductId = productID;
+                return View(order);
+            }
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
