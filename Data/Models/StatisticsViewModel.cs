bc44405ed460555fe42d8a75fc566e9886dc261e
stephanie <stephanie.maqdis@icloud.com>
2022-04-17 23:30:14 +0200

    moms and statistics

---
+++ Data/Models/StatisticsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class StatisticsViewModel
+    {
+        public SortedDictionary<int, string> Orders { get; set; }
+        public double TotalVAT { get; set; }
+    }
+}
